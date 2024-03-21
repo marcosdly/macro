81a33f0873e10c4e7766f647de9399bfe3e9ae81
Marcos Delay <dev.marcosdly@gmail.com>
2024-03-20 22:45:39 -0300

    refactor(performance): replace properties by attributes

--- src/image_utils.py
+++ src/image_utils.py
@@ -7,27 +7,13 @@ class Rectangle:
   def __init__(self, top_left: Tuple[int, int], bottom_right: Tuple[int, int]):
     self.top_left = top_left
     self.bottom_right = bottom_right
+    self.both_ends = (self.top_left, self.bottom_right)
+    self.width = self.bottom_right[0] - self.top_left[0]
+    self.height = self.bottom_right[1] - self.top_left[1]
+    self.size = (self.width, self.height)
 
-  @property
-  def both_ends(self):
-    return (self.top_left, self.bottom_right)
-
-  @property
-  def width(self):
-    return self.bottom_right[0] - self.top_left[0]
-
-  @property
-  def height(self):
-    return self.bottom_right[1] - self.top_left[1]
-
-  @property
-  def size(self):
-    return (self.width, self.height)
-
-  @property
-  def center(self):
     pt = self.top_left
-    return (pt[0] + self.width // 2, pt[1] + self.height // 2)
+    self.center = (pt[0] + self.width // 2, pt[1] + self.height // 2)
 
   def draw_over(self, frame: MatLike, color: Scalar = (254, 0, 0), thickness: int = 2):
     cv.rectangle(frame, *self.both_ends, color, thickness)
